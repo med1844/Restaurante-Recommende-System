5f5505a7ec2b5120e7bb71ba8ac6737743a1bb08
med <medioqrity@gmail.com>
2023-12-07 18:08:43 -0500

    make AutoSVD++ compatible with recommender trait

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from svd_sparse_cf import SVDSparseCollaborativeFilteringRecommender
+from AutoSVDpp.auto_svd_pp import AutoSVDPPRecommender
 from data_utils import load_data
 from random import choice, seed
 from typing import List, Any, Tuple
 from random_recommender import RandomRecommender
 import json
+import os
 
 seed(6220)
 
@@ -14,20 +16,26 @@ def split(data: List[Any], ratio: float) -> Tuple[List[Any], List[Any]]:
 
 
 def main():
-    user = json.load(open("./data/sample_users.json", "r"))
-    business = json.load(open("./data/sample_business.json", "r"))
-    review_train = json.load(open("./data/sample_reviews_train.json", "r"))
-    review_test = json.load(open("./data/sample_reviews_test.json", "r"))
-    model = SVDSparseCollaborativeFilteringRecommender(user, business, review_train, 5)
-    model.fit()
-    print("svd sparse, k = 5")
-    print("train rmse", model.eval(review_train))
-    print("test rmse", model.eval(review_test))
+    # user = json.load(open("./data/sample_users.json", "r"))
+    # business = json.load(open("./data/sample_business.json", "r"))
+    # review_train = json.load(open("./data/sample_reviews_train.json", "r"))
+    # review_test = json.load(open("./data/sample_reviews_test.json", "r"))
 
-    print("random")
-    model = RandomRecommender(business)
-    print("train rmse", model.eval(review_train))
-    print("test rmse", model.eval(review_test))
+    model = AutoSVDPPRecommender(os.path.abspath("./AutoSVDpp/datasets/subsets/restaurant_features_encoded.csv"))
+    model.predict("JyzLjUFEIW3epNlHI6Oa6Q")
+    # for r in review_test:
+    #     print(model.predict(r["user_id"]))
+
+    # model = SVDSparseCollaborativeFilteringRecommender(user, business, review_train, 5)
+    # model.fit()
+    # print("svd sparse, k = 5")
+    # print("train rmse", model.eval(review_train))
+    # print("test rmse", model.eval(review_test))
+
+    # print("random")
+    # model = RandomRecommender(business)
+    # print("train rmse", model.eval(review_train))
+    # print("test rmse", model.eval(review_test))
 
 
 if __name__ == "__main__":
