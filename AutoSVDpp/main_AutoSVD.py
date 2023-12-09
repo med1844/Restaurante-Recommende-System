6fe085e3df3081e6eb505b906189c3c8bc0a1506
Yuxin Cao <helen.cao6322@gmail.com>
2023-12-09 01:43:01 -0500

    Add files via upload

--- AutoSVDpp/main_AutoSVD.py
+++ AutoSVDpp/main_AutoSVD.py
@@ -3,8 +3,10 @@ from models.AutoSVD import AutoSVD
 import numpy as np
 import os
 
-train_data, test_data = LoadData().loadSmallDataSet()
-autosvd = AutoSVD(path_of_feature_file="datasets/subsets/restaurant_features_encoded.csv")
+# train_data, test_data = LoadData().loadSmallDataSet()
+train_data, test_data = LoadData().loadSampleDataSet()
+# autosvd = AutoSVD(path_of_feature_file="datasets/smallsets/restaurant_features_encoded.csv", epochs=30)
+autosvd = AutoSVD(path_of_feature_file="datasets/samplesets/restaurant_features_encoded.csv", epochs=30)
 autosvd.train(train_data=train_data, test_data=test_data)
 # autosvd.evaluate(test_data)
 autosvd.save_model()
--- AutoSVDpp/main_AutoSVDpp.py
+++ AutoSVDpp/main_AutoSVDpp.py
@@ -1,8 +1,10 @@
 from utils.LoadDataSet import LoadData
 from models.AutoSVDpp import *
 
-train_data, test_data = LoadData().loadSmallDataSet()
-autosvdpp = AutoSVDpp(path_of_feature_file="datasets/subsets/restaurant_features_encoded.csv")
+# train_data, test_data = LoadData().loadSmallDataSet()
+train_data, test_data = LoadData().loadSampleDataSet()
+# autosvdpp = AutoSVDpp(path_of_feature_file="datasets/smallsets/restaurant_features_encoded.csv")
+autosvdpp = AutoSVDpp(path_of_feature_file="datasets/samplesets/restaurant_features_encoded.csv")
 autosvdpp.train(train_data=train_data, test_data=test_data)
 # autosvdpp.evaluate(test_data)
 autosvdpp.save_model()
