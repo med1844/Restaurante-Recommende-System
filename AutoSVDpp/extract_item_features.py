45138295a27efdca33fdb8f54fb203901dea8f82
Yuxin Cao <helen.cao6322@gmail.com>
2023-12-09 07:54:29 -0500

    Add files via upload

--- AutoSVDpp/extract_item_features.py
+++ AutoSVDpp/extract_item_features.py
@@ -1,7 +1,12 @@
 from utils.ContractiveAutoEncoder import CAE
+import numpy as np
 
 cae = CAE()
-cae.train()
+losses = cae.train()
 cae.save_model()
 cae.load_model()
 cae.extract_features()
+
+float_array = np.array(losses)
+file_path_npy = 'predictions/cae_losses.npy'
+np.save(file_path_npy, float_array)
