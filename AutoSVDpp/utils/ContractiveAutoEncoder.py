ed6c75ccf953cf50aeb37713416dc1faf7d413d3
Yuxin Cao <helen.cao6322@gmail.com>
2023-12-09 01:41:02 -0500

    Add files via upload

---
+++ AutoSVDpp/utils/ContractiveAutoEncoder.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+import os
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+import torch
+from torch import miopen_depthwise_convolution, nn, optim
+from torch.utils.data import DataLoader, TensorDataset
+
+class CAE():
+    PATH_SMALL = 'datasets/smallsets/'
+    PATH_SAMPLE = 'datasets/samplesets/'
+
+    def __init__(self, path=PATH_SAMPLE):
+        filename = "sample_business.json"
+        df_business = pd.read_json(os.path.join(path, filename))
+        df_business.to_csv(os.path.join(path, 'restaurant_features.csv'))
+        self.df_business = df_business
+
+    def train(self, path=PATH_SAMPLE, epochs=30):
+        ## Step 1: Load and Preprocess Data
+        # Load the dataset
+        df = pd.read_csv(os.path.join(path, 'restaurant_features.csv'))
+        
+        # Assuming that the first two columns are 'business_id' and 'name'
+        categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
+        categorical_cols.remove('business_id')
+        categorical_cols.remove('name')
+        numeric_cols = df.select_dtypes(include=[np.number]).columns.tolist()
+        numeric_cols.remove('Unnamed: 0')
+
+        # One-hot encoding for categorical features
+        onehot = OneHotEncoder(sparse=False)
+        categorical_data = onehot.fit_transform(df[categorical_cols])
+
+        # Normalize the numeric features
+        scaler = MinMaxScaler()
+        numeric_data = scaler.fit_transform(df[numeric_cols])
+
+        # Combine categorical and numeric features
+        combined_features = np.hstack((categorical_data, numeric_data))
+
+        # Convert to PyTorch tensor
+        features_tensor = torch.tensor(combined_features, dtype=torch.float)
+        self.features_tensor = features_tensor
+
+        ## Step 2: Build the Contractive Autoencoder
+        class ContractiveAutoencoder(nn.Module):
+            def __init__(self, input_dim, encoding_dim=10):
+                super(ContractiveAutoencoder, self).__init__()
+                self.encoder = nn.Sequential(
+                    nn.Linear(input_dim, encoding_dim),
+                    nn.ReLU())
+                self.decoder = nn.Sequential(
+                    nn.Linear(encoding_dim, input_dim),
+                    nn.Sigmoid())
+
+            def forward(self, x):
+                encoded = self.encoder(x)
+                decoded = self.decoder(encoded)
+                return encoded, decoded
+
+        # Instantiate the model
+        input_dim = features_tensor.shape[1]
+        model = ContractiveAutoencoder(input_dim)
+
+        ## Step 3: Train the Autoencoder
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+        # Create DataLoader
+        dataset = TensorDataset(features_tensor, features_tensor)
+        data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
+
+        # Train the model
+        for epoch in range(epochs):
+            for inputs, targets in data_loader:
+                optimizer.zero_grad()
+                _, decoded = model(inputs)
+                loss = criterion(decoded, targets)
+                loss.backward()
+                optimizer.step()
+            print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+
+        self.model = model
+
+    def extract_features(self, path=PATH_SAMPLE):
+        self.model.eval()
+        with torch.no_grad():
+            encoded_features, _ = self.model(self.features_tensor)
+
+        # Convert to DataFrame
+        encoded_features_df = pd.DataFrame(encoded_features.numpy())
+        print(encoded_features_df)
+
+        # Save the encoded features to a CSV file
+        encoded_features_df.to_csv(os.path.join(path, 'restaurant_features_encoded.csv'))
+
+    def save_model(self, path=PATH_SAMPLE):
+        torch.save(self.model.state_dict(), os.path.join(path, 'CAEModel.pth'))
+
+    def load_model(self, path=PATH_SAMPLE):
+        state_dict = torch.load(os.path.join(path, 'CAEModel.pth'))
+        self.model.load_state_dict(state_dict)
--- AutoSVDpp/utils/YelpRecommendationGenerator.py
+++ AutoSVDpp/utils/YelpRecommendationGenerator.py
@@ -1,13 +1,13 @@
-from ..models.AutoSVD import AutoSVD
-from data_utils import load_data
+from models.AutoSVD import AutoSVD
+from utils.data_utils import load_data
 import pandas as pd
 import numpy as np
 from random import randint
 import os
 
 class YelpRecommendationGenerator():
-    PATH_SMALL = 'datasets/subsets/'
-    PATH_FULL= 'datasets/fullsets/'
+    PATH_SMALL = 'datasets/smallsets/'
+    PATH_SAMPLE = 'datasets/samplesets/'
 
     def __init__(self, model):
         '''
@@ -43,42 +43,46 @@ class YelpRecommendationGenerator():
         
         return df_business, df_review
 
-    def load_full_dataset(self, path=PATH_FULL):
+    def load_sample_dataset(self, path=PATH_SAMPLE):
         '''
-        Load the full Yelp restaurant dataset
+        Load the sample Yelp restaurant subset
         :param path: path of the dataset
         :return: business and review data frames
         '''
-        filenames = ("yelp_academic_dataset_user.json", "yelp_academic_dataset_business.json", "yelp_academic_dataset_review.json")
+        filenames = ("sample_users.json", "sample_business.json",
+             "sample_reviews_train.json", "sample_reviews_test.json")
         
         # Load and preprocess the business and review subsets
-        user_subset, business_subset, review_subset = load_data(path, filenames)
-        df_user = pd.DataFrame(user_subset)
-        df_business = pd.DataFrame(business_subset)
-        df_review = pd.DataFrame(review_subset)
-        df_review['user_idx'] = df_review.groupby(['user_id']).ngroup()
-        df_review['business_idx'] = df_review.groupby(['business_id']).ngroup()
+        df_user = pd.read_json(os.path.join(path, filenames[0]))
+        df_business = pd.read_json(os.path.join(path, filenames[1]))
+        df_review_train = pd.read_json(os.path.join(path, filenames[2]))
+        df_review_test = pd.read_json(os.path.join(path, filenames[3]))
+
         self.df_user = df_user
         self.df_business = df_business
-        self.df_review = df_review
+        self.df_review_train = df_review_train
+        self.df_review_test= df_review_test
 
-        n_users = df_review.user_id.unique().shape[0]
-        n_items = df_review.business_id.unique().shape[0]
+        unique_user_ids = pd.concat([df_review_train['user_id'], df_review_test['user_id']]).unique()
+        unique_business_ids = pd.concat([df_review_train['business_id'], df_review_test['business_id']]).unique()
+        user_id_to_index = {user_id: index for index, user_id in enumerate(unique_user_ids, start=0)}
+        business_id_to_index = {business_id: index for index, business_id in enumerate(unique_business_ids, start=0)}
+        
+        self.user_id_to_index = user_id_to_index 
+        self.business_id_to_index = business_id_to_index
+
+        df_review_train['user_idx'] = df_review_train['user_id'].map(user_id_to_index)
+        df_review_test['user_idx'] = df_review_test['user_id'].map(user_id_to_index)
+        df_review_train['business_idx'] = df_review_train['business_id'].map(business_id_to_index)
+        df_review_test['business_idx'] = df_review_test['business_id'].map(business_id_to_index)
+
+        n_users = len(unique_user_ids)
+        n_items= len(unique_business_ids)
         self.n_users = n_users
         self.n_items = n_items
         print("Number of users=" + str(n_users) + "; Number of items=" + str(n_items))  
         
-        return df_business, df_review
-
-    def get_user_df(self, user_id: str):
-        # Find the user_idx for the given user_id
-        user_idx = self.df_review[self.df_review['user_id'] == user_id]['user_idx'].iloc[0]
-
-        # Filter df_review using the user_idx to create user_df
-        user_df = self.df_review[self.df_review['user_idx'] == user_idx]
-        user_df = user_df.groupby(['user_idx', 'business_idx']).agg({'stars': 'mean'}).reset_index()
-
-        return user_df
+        return df_user, df_business, df_review_train, df_review_test
 
     def generate_sample_user(self):
         '''
@@ -86,32 +90,33 @@ class YelpRecommendationGenerator():
         :return: dataframe that contains information for the sample user
         '''
         sample_user_idx = randint(0, self.n_users-1)
-        sample_user_idx = 76
-        sample_df = self.df_review.loc[self.df_review["user_idx"] == sample_user_idx]
+        # sample_user_idx = 76
+        sample_df = self.df_review_test.loc[self.df_review_test["user_idx"] == sample_user_idx]
         user_df = sample_df.groupby(['user_idx', 'business_idx']).agg({'stars': 'mean'}).reset_index()
 
         # print the sampled user's profile
-        user_id = self.df_review[self.df_review['user_idx'] == sample_user_idx]['user_id'].iloc[0]
+        user_id = self.df_review_test[self.df_review_test['user_idx'] == sample_user_idx]['user_id'].iloc[0]
         user_row = self.df_user[self.df_user['user_id'] == user_id]
-        print("\n", "============== predict ratings of user ", sample_user_idx, " ==============", "\n")
+        print("\n", "=============== generate sample user ", user_id, " ===============", "\n")
         print(user_row)
 
-        return user_df
+        return user_id, user_df
 
-    def predict_stars_for_sample_user(self, user_df):
+    def predict_stars_for_sample_user(self, user_id):
         '''
         Predict the ratings the user will give to each business
         :param user_df: dataframe that contains information for the sample user
         :return: dataframe that contains predicted ratings of the sample user to all businesses (in desceding order)
         '''
         avg_rating = self.model.average_rating
-        u = user_df['user_idx'][0]
+        # u = user_df['user_idx'][0]
+        u = self.user_id_to_index[user_id]
         b_u = self.model.B_U[u]
         U_u = self.model.U[:, u]
         item_features = self.model.readItemFeature()
 
         predicted_ratings = []
-        print("\n", "========= predict user's rating of each business =========", "\n") 
+        # print("\n", "========= predict user's rating of each business =========", "\n") 
         for i in range(0, self.n_items):
           b_i = self.model.B_I[i]
           V_i = self.model.V[:, i]
@@ -126,35 +131,68 @@ class YelpRecommendationGenerator():
             predicted_rating = self.model.predict(avg_rating, b_u, b_i, U_u, V_i, beta * item_features[i])
           
           predicted_ratings.append(predicted_rating)
-          print("business_idx =", i, " predicted_star =", predicted_rating)
+          # print("business_idx =", i, " predicted_star =", predicted_rating)
 
-        # construct df for predictions
-        prediction_df= pd.DataFrame(predicted_ratings)
-        prediction_df['business_idx'] = list(range(0, self.n_items))
-        prediction_df = prediction_df.merge(self.df_review[['business_idx', 'business_id']], on='business_idx', how='left')
-        prediction_df = prediction_df.drop_duplicates()
-        prediction_df = prediction_df.rename(columns={0: "stars"})
-
-        # sort the df in descending order of predicted ratings
-        sorted_df = prediction_df.sort_values(by='stars', ascending=False)
+        # Invert the business_map dictionary to map indices to business IDs
+        index_to_business_id = {v: k for k, v in self.business_id_to_index.items()}
+        # Create a DataFrame from the inverted dictionary
+        df = pd.DataFrame(list(index_to_business_id.items()), columns=['business_idx', 'business_id'])
+        # Add the ratings to the DataFrame
+        df['stars'] = df['business_idx'].apply(lambda x: predicted_ratings[x])
+        # Sort the DataFrame based on ratings in descending order
+        sorted_df = df.sort_values(by='stars', ascending=False)
         self.sorted_df = sorted_df
 
         return sorted_df
 
-    def generate_top_k_recommendations(self, k=10):
+    def predict_stars_for_all_users(self):
+        test_matrix = np.zeros((self.n_users, self.n_items))
+        
+        avg_rating = self.model.average_rating
+        item_features = self.model.readItemFeature()
+        beta = self.model.beta
+
+        for u in range(test_matrix .shape[0]):  # Iterate over users
+            b_u = self.model.B_U[u]
+            U_u = self.model.U[:, u]
+            for i in range(test_matrix .shape[1]):  # Iterate over items
+                b_i = self.model.B_I[i]
+                V_i = self.model.V[:, i]
+                if hasattr(self.model, 'Y'):
+                  # AutoSVD++
+                  dot = np.dot(U_u.T, (V_i + beta * item_features[i]))
+                  predicted_rating = avg_rating + b_u + b_i + dot
+                else:
+                  # AutoSVD
+                  predicted_rating = self.model.predict(avg_rating, b_u, b_i, U_u, V_i, beta * item_features[i])
+                test_matrix[u][i] = predicted_rating
+
+        return test_matrix
+
+    def generate_top_k_recommendations(self, user_id, business_ids=None, k=10):
         '''
-        Generate the top k recommendations for the sample user
+        Generate the top k recommendations for the given user_id among given business_ids
         :return: top k recommended restaurants
         '''
+        if business_ids == None:
+           business_ids = list(self.business_id_to_index.keys())
+        sorted_df = self.predict_stars_for_sample_user(user_id = user_id)
         recommendations_df = pd.DataFrame(columns=self.df_business.columns)
 
-        for index, row in self.sorted_df.head(k).iterrows():
+        i = 0
+        for index, row in sorted_df.iterrows():
+          if i == k:
+              break;
           business_id = row['business_id']
+          if business_id not in business_ids:
+              continue;
           new_row = self.df_business[self.df_business['business_id'] == business_id]
           recommendations_df = pd.concat([recommendations_df, new_row], ignore_index=True)
+          i = i + 1
         self.recommendations_df = recommendations_df
+        stars_df = sorted_df[:k]
         
-        return recommendations_df
+        return stars_df, recommendations_df
 
     def save_recommendations_autosvd(self, directory='predictions/'):
         # Save the ratings and recommendations dataframes to the file
@@ -179,4 +217,3 @@ class YelpRecommendationGenerator():
         recommendations_df = pd.read_csv(os.path.join(directory, "recommendations_df_pp.csv"))
         
         return sorted_df, recommendations_df
-
